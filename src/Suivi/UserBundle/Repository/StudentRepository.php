3b12664e3d03baae1b79a28d555a3d07393a0760
Jacob Delcroix <delcroix.jacob@gmail.com>
2012-10-16 13:20:27 +0200

    Better typo for query / Missing documentation

--- src/Suivi/UserBundle/Repository/StudentRepository.php
+++ src/Suivi/UserBundle/Repository/StudentRepository.php
@@ -3,18 +3,26 @@ namespace Suivi\UserBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Student Repository
+ *
+ * @author Jacob Delcroix <delcroix.jacob@gmail.com>
+ */
 class StudentRepository extends EntityRepository
 {
+	/**
+	 * Get the list of all students and their complementary informations (professor, tutor, contract type)
+	 *
+	 * @return array
+	 */
 	public function getList()
 	{
 		$queryBuilder = $this
-			->createQueryBuilder('a')
-			->leftJoin('a.professor', 'b')
-			->addSelect('b')
-			->leftJoin('a.tutor', 'c')
-			->addSelect('c')
-			->leftJoin('a.contractType', 'd')
-			->addSelect('d');
+			->createQueryBuilder('student')
+			->select('student, professor, tutor, contract_type')
+			->leftJoin('student.professor', 'professor')
+			->leftJoin('student.tutor', 'tutor')
+			->leftJoin('student.contractType', 'contract_type');
 		return $queryBuilder->getQuery()->getResult();
 	}
 }
